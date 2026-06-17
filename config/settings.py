2fc07f620565d9d4f4e85dbe99ac6ed337beba51
Abhishek Kumar <abhish2882001@gmail.com>
2026-06-17 19:16:48 +0530

    fix static files

--- config/settings.py
+++ config/settings.py
@@ -119,13 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / "website" / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / "staticfiles"
