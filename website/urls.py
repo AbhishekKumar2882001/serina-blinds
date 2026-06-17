e6bd049755322fa898d5a29672002739d0ad7dce
Abhishek Kumar <abhish2882001@gmail.com>
2026-06-17 19:00:15 +0530

    fix static media

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -21,3 +23,6 @@ urlpatterns = [
     path('home-enquiry/', views.home_enquiry, name='home_enquiry'),
     path('quote-request/', views.quote_request, name='quote_request'),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
